3cc6b13360741f6dccb8d087d5f01bdd08af9e00
MarioCesarBais <mariocfbais@gmail.com>
2023-08-14 11:00:04 -0300

    Eliminado Bug do Loop para clique em Add-Noticia

--- middleware/is-adm.js
+++ middleware/is-adm.js
@@ -2,4 +2,5 @@ module.exports = (req, res, next) => {
     if (!(req.session.user && req.session.user.role === 'adm')) {
         return res.render('401', { pageTitle: 'Operação Não Autorizada', path: '/' });
     }
+    next()
 }
