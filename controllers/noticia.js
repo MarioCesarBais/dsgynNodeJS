c70dec01dd215280143c0467ed01e4a70eea7ce1
MarioCesarBais <mariocfbais@gmail.com>
2023-08-15 07:18:08 -0300

    Atualizado package.json para atualizar cabeçalhos, node engine, eliminar pacotes como stripe e sendgrid

--- controllers/noticia.js
+++ controllers/noticia.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PDFDocument = require('pdfkit');
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const Noticia = require('../models/noticia');
 const Order = require('../models/order');
