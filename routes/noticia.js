1e5456c52b513fc8d7f1681f85e0d0600aff7ab1
MarioCesarBais <mariocfbais@gmail.com>
2023-08-16 12:10:16 -0300

    Tradução das views. Eliminação de constantes desnecessárias.

--- routes/noticia.js
+++ routes/noticia.js
@@ -1,9 +1,9 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 
 const noticiaController = require('../controllers/noticia');
-const isAuth = require('../middleware/is-auth');
+// const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -13,16 +13,5 @@ router.get('/', noticiaController.getNoticias);
 
 router.get('/noticias/:noticiaId', noticiaController.getNoticia);
 
-// router.get('/cart', isAuth, noticiaController.getCart);
-
-// router.post('/cart', isAuth, noticiaController.postCart);
-
-// router.post('/cart-delete-item', isAuth, noticiaController.postCartDeleteNoticia);
-
-// router.get('/checkout', isAuth, noticiaController.getCheckout);
-
-// router.get('/orders', isAuth, noticiaController.getOrders);
-
-// router.get('/orders/:orderId', isAuth, noticiaController.getInvoice);
 
 module.exports = router;
