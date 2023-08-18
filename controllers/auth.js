102380d32b2bd4e577c23922b26b94af8d0e1839
MarioCesarBais <mariocfbais@gmail.com>
2023-08-18 15:32:22 -0300

    Tentativa de acabar com bug em produção, que não se repete em desenvolvimento: reset de senha de e-mail inválido

--- controllers/auth.js
+++ controllers/auth.js
@@ -265,6 +265,7 @@ exports.postReset = (req, res, next) => {
           });
       })
       .catch(err => {
+        console.log(err)
         const error = new Error(err);
         error.httpStatusCode = 500;
         return next(error);
