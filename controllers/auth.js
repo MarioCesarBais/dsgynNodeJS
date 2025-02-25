0a109ccd18decf5d2c13c8942b5a75c2835ab7f5
MarioCesarBais <mariocfbais@gmail.com>
2025-02-25 12:06:40 -0300

    Alterações no github não conseguem alterar código no Vercel

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,15 +8,9 @@ const User = require("../models/user");
 
 const appName = process.env.APP_NAME;
 const appUrl = appName
-  ? `https://${appName}.vercel.app/`
+  ? `https://${appName}`
   : `http://localhost:${process.env.PORT}/`;
 console.log("11", appUrl, process.env.APP_NAME);
-
-// const appUrl = process.env.VERCEL_URL
-//   ? `https://${process.env.VERCEL_URL}/`
-//   : `http://localhost:${process.env.PORT}/`;
-// console.log("URL da aplicação:", appUrl);
-
 
 let mailOptions = {
   from: "Mario Cesar Bais <mariocfbais@gmail.com>",
